268b91da3594ae4783e512ae6b1aa67dd515ba73
Ekanshsharma@18 <ekanshsharmacse11@gmail.com>
2017-03-11 18:50:35 +0530

    Add scope to filter product.
    1. Based on category and tag.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   validates_presence_of :sku_id
   validates :sku_id, uniqueness: true
+  # scope :category_search, ->(val) { where("'#{val}' = ANY (categories)") }
+  # scope :tag_search, ->(val) { where("'#{val}' = ANY (tags)") }
 end
