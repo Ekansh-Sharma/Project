346f2cb575f553260b38cec4f64d6b01629bfd84
Ekanshsharma@18 <ekanshsharmacse11@gmail.com>
2017-03-11 18:16:51 +0530

    Add crate action to respond to api's.
    1. Use safe params to filter attributes.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,29 @@
 module Api
   class V1::ProductsController < ApplicationController
+    skip_before_filter  :verify_authenticity_token
+    protect_from_forgery :except => [:create]
+
+    def create
+      @product = Product.new(product_params)
+      respond_to do |format|
+
+        if @product.save
+          format.json { render json: {message: 'Successfully Created.'}, status: :created }
+        else
+          format.json { render json: { erorrs: @product.errors }, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:expire_date, :name, :sku_id,
+                                      { categories: [] },
+                                      { tags: [] },
+                                      { images: [:img_path] },
+                                      :price,
+                                      :description)
+    end
   end
 end
