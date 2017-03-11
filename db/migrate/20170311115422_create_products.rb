82835fa8905611ff0f1f686eff6577d3c6ff1257
Ekanshsharma@18 <ekanshsharmacse11@gmail.com>
2017-03-11 17:47:53 +0530

    Define product table.
    1. Price is in decimal format.
    2. Categories and Tags are  array.
    3. Images as json.

--- db/migrate/20170311115422_create_products.rb
+++ db/migrate/20170311115422_create_products.rb
@@ -1,7 +1,15 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.string :sku_id, index: true, unique: true
+      t.decimal :price, precision: 15, scale: 10
+      t.text :description
+      t.text :categories, array: true, default: []
+      t.text :tags, array: true, default: []
+      t.json :images, default: {}
+      t.datetime :expire_date
+      t.boolean :admin_approved
       t.timestamps null: false
     end
   end
