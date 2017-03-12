cb2427338503913364ef9bf8d030f249252536df
Ekanshsharma@18 <ekanshsharmacse11@gmail.com>
2017-03-12 18:58:06 +0530

    Add form validation.
    1. Add category validtion.
    2. Add Tag validation.

--- app/assets/javascripts/admin/products.js
+++ app/assets/javascripts/admin/products.js
@@ -1,15 +1,42 @@
 var product = function(){
   var addTags = function(event){
-    let html = '<div><label>Tag Name</label><input type="text" name="product[tags][]"><a onclick="remove(this)" href="#">Remove</a><br/></div>'
+    let html = '<div><label>Tag Name</label><input type="text" name="product[tags][]" class ="validateTags"><a onclick="remove(this)" href="#">Remove</a><br/></div>'
     $("#tags").append(html);
     return event.preventDefault();
   }
 
   var addCategories = function(event) {
-   let html = '<div><label>Category Name</label><input type="text" name="product[categories][]"><a onclick="remove(this)" href="#">Remove</a><br/></div>'
+   let html = '<div><label>Category Name</label><input type="text" name="product[categories][]" class ="validateCategories"><a onclick="remove(this)" href="#">Remove</a><br/></div>'
     $("#categories").append(html);
     return event.preventDefault();
   }
-  return { addTags: addTags, addCategories: addCategories};
+
+  var formValidate = function(event){
+    var flag = true;
+    $(".validateCategories").filter(function() {
+      if(checkEmpty(this, 'Category')){
+        flag = false;
+        return false;
+      }
+    });
+    if(flag){
+      $(".validateTags").filter(function() {
+        if(checkEmpty(this, 'Tag')){
+          flag = false;
+          return false;
+        }
+      });
+    };
+    return flag;
+  }
+
+  var checkEmpty = function(that, name){
+    if($(that).val() == ""){
+        alert(name + ' is Empty');
+        return true;
+    }
+    return false;
+  }
+  return { addTags: addTags, addCategories: addCategories, formValidate: formValidate};
 }();
 
