5ab06fce9160d406c2ab24bffd73d25deb882e81
Ekanshsharma@18 <ekanshsharmacse11@gmail.com>
2017-03-12 19:01:01 +0530

    Remove blank values for tag and category.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,11 @@ class Product < ActiveRecord::Base
   validates :sku_id, uniqueness: true
   # scope :category_search, ->(val) { where("'#{val}' = ANY (categories)") }
   # scope :tag_search, ->(val) { where("'#{val}' = ANY (tags)") }
+  before_save :remove_blank_vales
+
+  private
+  def remove_blank_vales
+    categories.delete_if(&:blank?)
+    tags.delete_if(&:blank?)
+  end
 end
